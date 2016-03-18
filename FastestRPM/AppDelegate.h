14db85e6d71c289f0a65242612b975e2bf74c146
neliSun <nelson.chow91.nc@gmail.com>
2016-03-17 17:36:43 -0700

    Initial Commit

---
+++ FastestRPM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastestRPM
+//
+//  Created by Nelson Chow on 2016-03-17.
+//  Copyright © 2016 Nelson Chow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
